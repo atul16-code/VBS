d54d9783d8a84012856eed7895ec006e129e10e3
atul16-code <atulsapkale1608@gmail.com>
2025-12-30 13:28:54 +0530

    Initial Commit

---
+++ src/main/java/com/vbs/demo/models/History.java
@@ -0,0 +1,25 @@
+package com.vbs.demo.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.service.spi.InjectService;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class History {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    @Column(nullable = false)
+    String description;
+    @CreationTimestamp
+    @Column(nullable = false,updatable = false)
+    LocalDateTime date;
+}
